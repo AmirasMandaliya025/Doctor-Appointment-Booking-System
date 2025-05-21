6f7b6ff96d024e11224e80f0ce9b4049b3aa37ab
Amiras Mandaliya <amirasmandaliya99@gmail.com>
2025-05-21 19:05:02 +0530

    Header

---
+++ frontend/src/components/SpecialityMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SpecialityMenu = () => {
+  return (
+    <div id="speciality" className='flex flex-col items-center gap-4 py-16 text-[#262626]'>
+        <h1 class="text-3xl font-medium">Find by Speciality</h1>
+        <p class="sm:w-1/3 text-center text-sm">Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
+    </div>
+  )
+}
+
+export default SpecialityMenu
